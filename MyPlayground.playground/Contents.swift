7a031f6c0d5b1a3d737ec981734a66039928336e
Juan Saavedra <saavedrajj@Juans-MacBook-Pro.local>
2019-08-08 21:15:43 +0100

    2

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// var and let
+
+var str = "Hello, Playground"
+
+str = "Hi, playground"
+
+let otherStr = "asdasd" // otherStr cannot be changed
+
+// Strings
+
+let name = "Juan"
+
+print("Hello " + name)
+
+// Integer (whole number)
+
+var myInt = 8
+
+print(myInt * 2)
+
+print(myInt + 100)
+
+myInt = myInt + 1
+
+myInt = myInt / 5
+
+print("myInt has value \(myInt)")
+
+// My name is Rob and I am 35 years old
+
+let age = 45
+
+print("My name is " + name + " and I am \(age) years old" )
